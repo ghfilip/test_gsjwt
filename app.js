860956d906647b0c916cb46c0e367c33f4b7a024
Filip <filip@P5RO-PORT784.local>
2019-02-28 19:02:02 +0200

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 // app
 
 const express = require('express');
-// const { ApolloServer } = require('apollo-server-express');
-// const typeDefs = require('./data/schema');
-// const resolvers = require('./data/resolvers');
 const jwt = require('express-jwt');
 require('dotenv').config();
 const schema = require('./graphql/schema');
@@ -23,8 +20,6 @@ const auth = jwt({
 // graphql endpoint
 app.use(path, auth);
 app.use(path, schema);
-
-// server.applyMiddleware({ app, path });
 
 app.listen(port, () => {
   console.log(`The server is running on http://localhost:${port}${path}`);
